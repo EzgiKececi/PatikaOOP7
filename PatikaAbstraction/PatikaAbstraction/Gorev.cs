a84823811424893831f81e6d48f3a756382efc97
Ezgi Kececi Atalay <ezgikececii@gmail.com>
2024-08-14 11:00:45 +0300

    Add files via upload

---
+++ PatikaAbstraction/PatikaAbstraction/Gorev.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatikaAbstraction
+{
+    public abstract class Person // Ortak özelliklerin olduğu abstract sınıf oluşturuldu
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Department { get; set; }
+        public string Job { get; set; }
+
+        public abstract void Position();
+        
+        public virtual void PersonInfo() 
+        {
+            Console.WriteLine($"{Name} {Surname} {Department} ");
+        }
+    }
+
+    public class Employee : Person
+    {
+        public Employee(string name, string surname, string department,string job) 
+        {
+            Name = name;
+            Surname = surname;
+            Department = department;
+            Job = job;
+
+        }
+        public override void PersonInfo()
+        {
+            base.PersonInfo();
+        }
+
+        public override void Position()
+        {
+            Console.WriteLine($"{Job} olarak çalışmakta.");
+        }
+    } }
---
+++ PatikaAbstraction/PatikaAbstraction/Program.cs
@@ -0,0 +1,11 @@
+﻿using PatikaAbstraction;
+
+public class Program
+{
+    public static void Main()
+    {
+        Employee employee = new Employee("Ezgi", "Keçeci", "Üretim", "Makine Mühendisi");
+        employee.PersonInfo();
+        employee.Position();
+    }
+}
